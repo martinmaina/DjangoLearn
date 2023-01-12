aa632f23bc0445b6a27f24375d5528cac59d7029
martinmaina <m0Lzixs3m0qy@gmail.com>
2023-01-12 15:53:37 +0300

    Update models

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from store.models import Product 
 
 def say_hello(request):
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product)
+        
     return render(request, 'hello.htm', {'name':'Martin'}) 
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(default='-')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -85,8 +85,8 @@ class Address(models.Model):
 
     # If the customer can have multiple addresses, then the relationship will be one-to-many
     # customer_a = models.ForeignKey(Customer,on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    zip = models.CharField(max_length=255)
+    # order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    # zip = models.CharField(max_length=255)
 
 
 class OrderItem(models.Model):
